6a0d5b2f954963d89f1fda6bb38ac1df60626cca
Wendell <wendellbarreto2007@gmail.com>
2026-04-22 12:36:23 -0300

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import AuthCallback from "./pages/AuthCallback";
 import Dashboard from "./pages/Dashboard";
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/auth" replace />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/auth/callback" element={<AuthCallback />} />
       <Route path="/dashboard" element={<Dashboard />} />
